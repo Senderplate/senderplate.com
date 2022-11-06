765c183338b32ef6b1075b3ce27cd69c3b177fce
Jin Su Park <jinsupark@icloud.com>
2022-11-07 01:18:02 +1300

    Add

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+// Initialize AlpineJS
+document.addEventListener('alpine:init', () => {
+  // Main
+  Alpine.data('main', () => ({
+    sidebar: true,
+    slidePanel: false,
+    userSlidePanel: false,
+
+    init: function () {},
+    showModal() {
+      this.modal = !this.modal;
+
+      var iframe = document.querySelector('.js-embedFrame') !== null;
+      if (iframe) {
+        var iframe = document.querySelector('.js-embedFrame');
+      }
+
+      iframe.src = 'https://www.youtube.com/embed/Zz6eOVaaelI?autoplay=1';
+    },
+  })),
+
+});
