be08017f449a650645fea7d57617c41a758aed13
Jin Su Park <jinsupark@icloud.com>
2022-11-18 01:35:24 +1300

    Add

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,87 @@ document.addEventListener('alpine:init', () => {
   }))
 
 });
+
+
+document.addEventListener(
+  'DOMContentLoaded',
+  function () {
+
+    // gsap.registerPlugin(ScrollTrigger);
+
+    // var tl = gsap.timeline({ repeat: 0, repeatDelay: 1 });
+    // tl.from('.gsap-carousel-item-3', { y: 200, opacity: 0, duration: 1 });
+
+    // tl.from(
+    //   '.gsap-carousel-item-2',
+    //   { y: 200, opacity: 0, duration: .4 },
+    //   '-=.5'
+    // );
+    // tl.from(
+    //   '.gsap-carousel-item-4',
+    //   { y: 200, opacity: 0, duration: .4 },
+    //   '-=1'
+    // );
+
+
+    // tl.from(
+    //   '.gsap-carousel-item-1',
+    //   { y: 200, opacity: 0, duration: .4 },
+    //   '-=.5'
+    // );
+    // tl.from(
+    //   '.gsap-carousel-item-5',
+    //   { y: 200, opacity: 0, duration: .4 },
+    //   '-=1'
+    // );
+
+    // tl.play();
+
+    gsap.to('.gsap-carousel', {
+      duration: 1,
+      opacity: 1,
+    });
+
+    gsap.from('.gsap-carousel-item-3', {
+      y: 600,
+      duration: 2,
+      ease: "expo.out",
+    });
+
+
+    gsap.from('.gsap-carousel-item-2', {
+      x: 500,
+      duration: 2,
+      opacity: 0,
+      ease: "expo.out",
+      delay: 1
+    });
+    gsap.from('.gsap-carousel-item-4', {
+      x: -500,
+      duration: 2,
+      opacity: 0,
+      ease: "expo.out",
+      delay: 1,
+    });
+
+
+    gsap.from('.gsap-carousel-item-1', {
+      x: 1000,
+      duration: 2,
+      opacity: 0,
+      ease: "expo.out",
+      delay: 1.2,
+    });
+    gsap.from('.gsap-carousel-item-5', {
+      x: -1000,
+      duration: 2,
+      opacity: 0,
+      ease: "expo.out",
+      delay: 1.2,
+    });
+
+    
+
+  },
+  false
+);
