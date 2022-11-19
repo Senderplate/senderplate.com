31ecdd27994845a914778f5c812139e8ec06283e
Jin Su Park <jinsupark@icloud.com>
2022-11-19 22:59:06 +1300

    Add

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,10 +15,10 @@ module.exports = {
         // '2xl': '6rem',
       },
       screens: {
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1280px',
+        // sm: '640px',
+        // md: '768px',
+        // lg: '1024px',
+        // xl: '1280px',
       },
     },
     fontWeight: {
